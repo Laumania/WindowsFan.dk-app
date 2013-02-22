4451188f200b8d9419facf22b3b319029abf7e8d
Laumania <mads@laumania.net>
2013-02-22 21:25:15 +0100

    Work in progress
    Put a lot of code in now, for reference, nothings working yet at all.

--- src/WindowsFanDkApp.TaskAgent/ScheduledAgent.cs
+++ src/WindowsFanDkApp.TaskAgent/ScheduledAgent.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.IO.IsolatedStorage;
 using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Resources;
 using AgFx;
 using Microsoft.Phone.Scheduler;
 using Microsoft.Phone.Shell;
@@ -50,8 +55,8 @@ namespace WindowsFanDkApp.TaskAgent
         protected override void OnInvoke(ScheduledTask task)
         {
             // get application tile
-            ShellTile tile = ShellTile.ActiveTiles.FirstOrDefault();
-            if (null != tile)
+            ShellTile activeLiveTile = ShellTile.ActiveTiles.FirstOrDefault();
+            if (null != activeLiveTile)
             {
                 var recentposts = DataManager.Current.Load<RecentPosts>(-1);
                 recentposts.PropertyChanged += (sender, args) =>
@@ -61,29 +66,73 @@ namespace WindowsFanDkApp.TaskAgent
                             var latestPost = recentposts.Posts.FirstOrDefault();
                             if (latestPost != null)
                             {
-                                // creata a new data for tile
-                                var data = new StandardTileData();
-                                // tile foreground data
-                                //data.Title = latestPost.TitlePlain;
-                                //data.BackgroundImage = new Uri("/Content/CommentsIcon.png", UriKind.Relative);
-                                //data.BackgroundImage = new Uri(latestPost.Thumbnail, UriKind.Absolute);
-                                //data.Count = 1;
-                                // to make tile flip add data to background also
-                                //data.BackTitle = "Secret text here";
+                                var bitmap = new BitmapImage();
+                                bitmap.CreateOptions = BitmapCreateOptions.None;
+                                bitmap.UriSource = new Uri(latestPost.Thumbnail, UriKind.Absolute);
+                                //bitmap.ImageFailed += (o, eventArgs) => NotifyComplete();
+                                //bitmap.ImageOpened += (o, eventArgs) =>
+                                //    {
+                                        Grid grid = new Grid();
+                                        Image img = new Image();
+                                        img.Source = bitmap;
 
-                                //Todo: http://windcape.posterous.com/how-to-generate-a-custom-live-tile-directly-o
-                                data.BackBackgroundImage = new Uri(latestPost.Thumbnail, UriKind.Absolute);
-                                data.BackContent = latestPost.TitlePlain;
-                                // update tile
-                                tile.Update(data);    
+                                        // add Image to Grid
+                                        grid.Children.Add(img);
+
+                                        TextBlock text = new TextBlock()
+                                        {
+                                            FontSize = 11,
+                                            Foreground = new SolidColorBrush(Colors.Black),
+                                            TextWrapping = TextWrapping.Wrap,
+                                        };
+
+                                        text.Text = "Test";
+                                        grid.Children.Add(text);
+
+
+                                        // this is our final image containing custom text and image
+                                        WriteableBitmap wbmp = new WriteableBitmap(173, 173);
+
+                                        // now render everything - this image can be used as background for tile
+                                        wbmp.Render(grid, null);
+                                        wbmp.Invalidate();
+
+                                        using (var stream = IsolatedStorageFile.GetUserStoreForApplication().CreateFile("/Shared/ShellContent/WindowsFanDkLiveTile.png"))
+                                        {
+                                            wbmp.SaveJpeg(stream, 173, 173, 0, 100);
+                                        }
+
+                                        var data = new StandardTileData();
+                                        data.BackBackgroundImage = new Uri("isostore:/Shared/ShellContent/tile.png", UriKind.Absolute);
+                                        //data.Title = "updated image";
+
+                                        activeLiveTile.Update(data);
+                                        
+                                    //};
+                                
+                                //// creata a new data for tile
+                                //var data = new StandardTileData();
+                                //// tile foreground data
+                                ////data.Title = latestPost.TitlePlain;
+                                ////data.BackgroundImage = new Uri("/Content/CommentsIcon.png", UriKind.Relative);
+                                ////data.BackgroundImage = new Uri(latestPost.Thumbnail, UriKind.Absolute);
+                                ////data.Count = 1;
+                                //// to make tile flip add data to background also
+                                ////data.BackTitle = "Secret text here";
+
+                                ////Todo: http://windcape.posterous.com/how-to-generate-a-custom-live-tile-directly-o
+                                //data.BackBackgroundImage = new Uri(latestPost.Thumbnail, UriKind.Absolute);
+                                //data.BackContent = latestPost.TitlePlain;
+                                //// update tile
+                                //tile.Update(data);    
                             }
-                            NotifyComplete();
+                            NotifyComplete();    
                         }
                     };
             }
 
 #if DEBUG
-	ScheduledActionService.LaunchForTest(task.Name, TimeSpan.FromSeconds(30));
+	ScheduledActionService.LaunchForTest(task.Name, TimeSpan.FromSeconds(5));
 	System.Diagnostics.Debug.WriteLine("Periodic task is started again: " + task.Name);
 #endif
         }
